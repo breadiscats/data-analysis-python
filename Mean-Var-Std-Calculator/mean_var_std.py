62658f62b8c680568726263e63bc15207012bbf1
breadiscats <fatihahnabilah27@gmail.com>
2025-01-23 00:10:02 +0900

    Update mean_var_std.py

--- Mean-Var-Std-Calculator/mean_var_std.py
+++ Mean-Var-Std-Calculator/mean_var_std.py
@@ -1,1 +1,22 @@
+import numpy as np
 
+def calculate(list):
+
+    if len(list) != 9:
+        raise ValueError ("List must contain nine numbers.")
+
+    # Make list with 3x3 NumPy array
+    matrix = np.array(list).reshape((3, 3))
+
+     # Dictionary
+    calculations = {
+
+            'mean' : [matrix.mean(0).tolist(), matrix.mean(1).tolist(), matrix.mean().tolist()],
+            'variance' : [matrix.var(0).tolist(), matrix.var(1).tolist(), matrix.var().tolist()],
+            'standard deviation' : [matrix.std(0).tolist(), matrix.std(1).tolist(), matrix.std().tolist()],
+            'max' : [matrix.max(0).tolist(), matrix.max(1).tolist(), matrix.max().tolist()],
+            'min' : [matrix.min(0).tolist(), matrix.min(1).tolist(), matrix.min().tolist()],
+            'sum' : [matrix.sum(0).tolist(), matrix.sum(1).tolist(), matrix.sum().tolist()]
+            }
+
+    return calculations
