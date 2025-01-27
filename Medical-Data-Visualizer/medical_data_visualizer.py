97a871d974d0860f3f6d83fe0115a49ae76440e8
breadiscats <fatihahnabilah27@gmail.com>
2025-01-27 21:19:18 +0900

    Update medical_data_visualizer.py

--- Medical-Data-Visualizer/medical_data_visualizer.py
+++ Medical-Data-Visualizer/medical_data_visualizer.py
@@ -20,7 +20,7 @@ def draw_cat_plot():
     df_cat = pd.melt(df, id_vars='cardio', value_vars=['cholesterol','gluc','smoke','alco','active','overweight'])
 
     # 6
-    df_cat = pd.DataFrame(df_cat.groupby(['cardio','variable','value']).size().reset_index(name = 'total'))
+    df_cat = df_cat.groupby(['cardio','variable','value']).size().reset_index(name = 'total')
     
     # 7
     fig = sns.catplot(x='variable',y='total',hue='value',col='cardio',data=df_cat,kind='bar').fig
@@ -53,7 +53,7 @@ def draw_heat_map():
     mask[np.triu_indices_from(mask)] = True
 
     # 14
-    fig, ax = plt.subplots(figsize=(12,9))
+    fig, ax = plt.subplots(figsize=(12,8))
 
     # 15
     ax = sns.heatmap(corr, center=0, vmax=0.25, annot=True, mask=mask, cmap='coolwarm', fmt='.1f', square=True)
@@ -61,3 +61,4 @@ def draw_heat_map():
     # 16
     fig.savefig('heatmap.png')
     return fig
+
